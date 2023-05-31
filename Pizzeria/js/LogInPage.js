d34de39c7bbc495e5384185894776ec331851c1c
Felice <felice.gammarrota@gmail.com>
2023-05-31 15:49:05 +0200

    first-commit

---
+++ Pizzeria/js/LogInPage.js
@@ -0,0 +1,40 @@
+function Login () {
+  var username = document.getElementById("uname").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("psw").value;
+
+  if(username == localStorage.getItem("username") 
+  && email == localStorage.getItem("email") 
+  && password == localStorage.getItem("password")){
+    window.location.replace("HomePage.html");
+  } else if (username != localStorage.getItem("username") 
+  && email != localStorage.getItem("email")) {
+    var error=document.getElementById("error-message");
+    error.style="color: red";
+    error.innerHTML="Utente inesistente!";
+  } else {
+    var error=document.getElementById("error-message");
+    error.style="color: red";
+    error.innerHTML="Ricontrolla i dati inseriti!";
+  }
+return false;
+}
+
+function Signin(){
+  var username = document.getElementById("uname").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("psw").value;
+
+  localStorage.setItem("username", username);
+  localStorage.setItem("email", email);
+  localStorage.setItem("password", password);
+}
+
+function showPsw() {
+    var x = document.getElementById("psw");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
